cb60f895e89d3088f255605ff9534f1177af19fc
brunovrportilho <brunovrportilho@gmail.com>
2018-05-10 18:05:56 -0400

    Update no pagina

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DevOps Essentials for Linux - Larigatona")
+        self.assertRegex(result.data.decode(), "DevOps Essentials for Linux")
 
 
 if __name__ == "__main__":
